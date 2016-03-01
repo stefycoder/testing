348644828f20659275a35dd27bc9792b5ccb42e4
stefycoder <merlinrupayvelazco@gmail.com>
2016-03-01 17:29:14 -0500

    respon

--- problema/js/menu-scroll.js
+++ problema/js/menu-scroll.js
@@ -1,2 +1,19 @@
+$(document).scroll(function() {    
+    var scroll = $(this).scrollTop();
+
+    if (scroll >= 200) {
+        $("#menu").addClass("active");
+    } else {
+        $("#menu").removeClass("active");
+    }
+
+});
+
+
+
+$(".navbar-toggle").click(function(){
+  $(this).toggleClass("open");
+});
+
 
 
